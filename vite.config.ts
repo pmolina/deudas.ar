1e46e5909892a248bd7f9e990bfe34b15124a841
pmolina <patriciomolina@gmail.com>
2026-03-02 20:49:03 -0300

    Adding gh-pages support

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/visual-dsf/',
   plugins: [react(), tailwindcss()],
 })
