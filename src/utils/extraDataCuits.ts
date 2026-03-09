2d85cdba1c718ff091a5fa16f075208dce272dbf
pmolina <patriciomolina@gmail.com>
2026-03-09 19:14:12 -0300

    Fixing type params, debt and cheques return different types

--- src/utils/extraDataCuits.ts
+++ src/utils/extraDataCuits.ts
@@ -2,10 +2,10 @@
  * Returns the subset of CUITs for which ExtraData should be fetched:
  * only those where at least one BCRA endpoint (debts or cheques) returned successfully.
  */
-export function getExtraDataCuits<T>(
+export function getExtraDataCuits<D, C>(
   cuits: string[],
-  debtSettled: PromiseSettledResult<T>[],
-  checksSettled: PromiseSettledResult<T>[],
+  debtSettled: PromiseSettledResult<D>[],
+  checksSettled: PromiseSettledResult<C>[],
 ): string[] {
   return cuits.filter((_, i) =>
     debtSettled[i]?.status === 'fulfilled' || checksSettled[i]?.status === 'fulfilled'
